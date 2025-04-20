78bd32c03afb21f1b0b8723331e93a81f983cd7d
mihir123895 <mp520814@gmail.com>
2025-04-20 22:20:52 +0530

    finalize

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 // To run locally use local host, for deployment use backend url
 // export const BASE_URL= 'http://localhost:1234/api/v1';  
-export const BASE_URL= 'https://travel-booking-system-azure.vercel.app/api/v1';
+export const BASE_URL= 'https://travel-booking-system-azure.vercel.app';
 
 // export const BASE_URL= 'https://tour-booking-system-bzkx.onrender.com/api/v1';
