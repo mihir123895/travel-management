3c709f28ac5abd1e307cd011e8660b806d1313c1
mihir123895 <mp520814@gmail.com>
2025-04-20 18:48:18 +0530

    complated deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,5 +18,5 @@ root.render(
         <App />
       </BrowserRouter>
     </AuthContextProvider>
-  </AdminContextProvider>
+  </AdminContextProvider> 
 );
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,5 @@
 // To run locally use local host, for deployment use backend url
 // export const BASE_URL= 'http://localhost:4000/api/v1';  
-export const BASE_URL= 'https://tour-booking-system-bzkx.onrender.com/';
+export const BASE_URL= 'https://travel-booking-system-azure.vercel.app/api/v1';
+
+// export const BASE_URL= 'https://tour-booking-system-bzkx.onrender.com/api/v1';
